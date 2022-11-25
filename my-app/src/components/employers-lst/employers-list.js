64df65d460f5d30e2d5ed25b997a6b57f92869b2
talant <talant.04asanov@gmail.com>
2022-11-25 21:31:49 +0600

    changes: added employer's deletion and star rating

---
+++ my-app/src/components/employers-lst/employers-list.js
@@ -0,0 +1,25 @@
+import EmployersListItem from '../employers-list-item/employers-list-item'
+import './employers-list.css'
+
+
+
+const EmployersList = ({data, onDelete}) => {
+
+    const elements = data.map(item => { 
+        const {id, ...itemProps} = item;
+        return (
+            <EmployersListItem 
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)} />
+        )
+    })
+
+    return ( 
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployersList;
---
+++ my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SerachPanel = () => {
+    return (
+        <input 
+            type="text" 
+            className="form-control search-input"
+            placeholder="Найти сотрудника"/>
+    )
+}
+
+export default SerachPanel;
